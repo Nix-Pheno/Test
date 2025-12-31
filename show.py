e2c7d759327d8c68e714cb1b8750bec075ac1069
Oraya <ainochan62@gmail.com>
2025-12-31 19:37:46 +0700

    main

---
+++ show.py
@@ -0,0 +1,14 @@
+import os
+
+present = os.path.join(os.path.join(os.environ['USERPROFILE'],'DESKTOP'))
+
+print(present)
+
+i = 1
+
+for i in range(6):
+    new = 'Muhehehehe'+ str(i)
+    f_path = os.path.join(present, new)
+    os.mkdir(f_path)
+    
+    i += 1
